6fa9647cac46e4e395bab9e3b5aa191147e71b4d
Jonathan Nava <jonathan.navaflores@gmail.com>
2020-11-04 14:57:30 -0600

    add code qty

---
+++ qty.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+require_once 'includes/connection.php';
+if (isset($_SESSION['idcliente'])) {
+    if (isset($_GET['cartItem']) && isset($_GET['cartItem']) == 'cart_item') {
+        $stmt = $connection->prepare('SELECT * FROM carrito');
+        $stmt->execute();
+        $stmt->store_result();
+        $rows = $stmt->num_rows;
+        echo $rows;
+    }
+}
