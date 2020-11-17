b564fdbc61aafe2080bf1c52d227c98a8af21907
Jonathan Nava <jonathan.navaflores@gmail.com>
2020-11-17 09:42:20 -0600

    actualización maxlength campos de form contacto

--- contacto.php
+++ contacto.php
@@ -16,123 +16,126 @@ if (isset($_SESSION['idcliente'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="/css/style.css"> -->
-    <title>EduTech</title>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="/css/style.css"> -->
+  <title>EduTech</title>
 </head>
+
 <body>
-    <div class="container">
-        <header>            
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="index.php"><img class="logo" src="images/logo.svg" alt="edutech" width="150px" ></a>                
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>              
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                  <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                    <li class="nav-item">
-                      <a class="nav-link" href="productos.php">Productos</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contacto.php">Contacto</a>
-                    </li>                  
-                  </ul>
-                </div>
-                <?php if(!empty($user)): ?>
-                  <div>
-                    <span class="text-white"><?= $user['email']; ?></span>
-                    <a class="btn btn-outline-success text-white" href="logout.php">Logout</a>
-                  </div>
-                  <?php else: ?>
-                  <div>
-                    <span class="text-white"></span>
-                    <a class="btn btn-outline-success text-white" href="login.php">Registro/Login</a>
-                  </div>
-                <?php endif; ?>
-              </nav>                        
-        </header>
-        <main>
-            <section class="jumbotron text-center">
-              <div class="container">
-                <h1 class="jumbotron-heading">Contacto</h1>
-              </div>
-            </section>
-            <div class="d-flex justify-content-center" id="mensajeContacto">
-                <form action="" method="POST" id="saveContact" class="p-2 w-50">
-                    <div class="form-group">
-                        <label for="InputName">Nombre</label>
-                        <input type="text" class="form-control" name="nombre" >
-                    </div>
-                    <div class="form-group">
-                      <label for="InputEmail">Email</label>
-                      <input type="email" class="form-control" name="email" >
-                    </div>
-                    <div class="form-group">
-                        <label for="InputPhone">Teléfono</label>
-                        <input type="text" class="form-control" name="telefono" >
-                    </div>
-                    <div class="form-group">
-                        <label for="Textarea">Comentarios</label>
-                        <textarea class="form-control" id="Textarea" name="comentaio" rows="3"></textarea>
-                    </div>                    
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-block p-2 ">Realizar Pedido</button>
-                    </div>
-                </form>
-            </div>
-        </main>
-        <footer class="pt-4 my-md-5 pt-md-5 border-top">
-            <div class="row">
-              <div class="col-12 col-md">
-                <img class="mb-2" src="images/logo_black.svg" alt="" width="150">
-                <small class="d-block mb-3 text-muted">©2020 EduTech</small>
-              </div>
-              <div class="col-6 col-md">
-                <h5>Cursos</h5>
-                <ul class="list-unstyled text-small">
-                  <li><a class="text-muted" href="#">Programación</a></li>
-                  <li><a class="text-muted" href="#">Diseño Web</a></li>
-                  <li><a class="text-muted" href="#">Desarrollo Web</a></li>
-                  <li><a class="text-muted" href="#">Seguridad Informática</a></li>
-                  <li><a class="text-muted" href="#">Redes</a></li>
-                </ul>
-              </div>
-              <div class="col-6 col-md">
-                <h5>Nosotros</h5>
-                <ul class="list-unstyled text-small">
-                  <li><a class="text-muted" href="#">Contacto</a></li>
-                  <li><a class="text-muted" href="#">Soporte</a></li>
-                  <li><a class="text-muted" href="#">Preguntas frecuentes</a></li>
-                </ul>
-              </div>
-            </div>
-        </footer>
-    </div>
-    
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script>
-        $(document).ready(function() {
-            $("#saveContact").submit(function(e) {
-                e.preventDefault();
-                $.ajax({
-                    url: 'guardar_contacto.php',
-                    method: 'post',
-                    data: $('form').serialize() + "&guardar_contacto=mensajeContacto",
-                    success: function(response) {
-                        $("#mensajeContacto").html(response);
-                    }
-                });
-            });
+  <div class="container">
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php"><img class="logo" src="images/logo.svg" alt="edutech" width="150px"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="productos.php">Productos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contacto.php">Contacto</a>
+            </li>
+          </ul>
+        </div>
+        <?php if (!empty($user)) : ?>
+          <div>
+            <span class="text-white"><?= $user['email']; ?></span>
+            <a class="btn btn-outline-success text-white" href="logout.php">Logout</a>
+          </div>
+        <?php else : ?>
+          <div>
+            <span class="text-white"></span>
+            <a class="btn btn-outline-success text-white" href="login.php">Registro/Login</a>
+          </div>
+        <?php endif; ?>
+      </nav>
+    </header>
+    <main>
+      <section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">Contacto</h1>
+        </div>
+      </section>
+      <div class="d-flex justify-content-center" id="mensajeContacto">
+        <form action="" method="POST" id="saveContact" class="p-2 w-50">
+          <div class="form-group">
+            <label for="InputName">Nombre</label>
+            <input type="text" class="form-control" name="nombre" maxlength="100" required>
+          </div>
+          <div class="form-group">
+            <label for="InputEmail">Email</label>
+            <input type="email" class="form-control" name="email" maxlength="100" required>
+          </div>
+          <div class="form-group">
+            <label for="InputPhone">Teléfono</label>
+            <input type="text" class="form-control" name="telefono" maxlength="12" required>
+          </div>
+          <div class="form-group">
+            <label for="Textarea">Comentarios</label>
+            <textarea class="form-control" id="Textarea" name="comentario" rows="3" maxlength="300" required></textarea>
+          </div>
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-block p-2 ">Realizar Pedido</button>
+          </div>
+        </form>
+      </div>
+    </main>
+    <footer class="pt-4 my-md-5 pt-md-5 border-top">
+      <div class="row">
+        <div class="col-12 col-md">
+          <img class="mb-2" src="images/logo_black.svg" alt="" width="150">
+          <small class="d-block mb-3 text-muted">©2020 EduTech</small>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Cursos</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Programación</a></li>
+            <li><a class="text-muted" href="#">Diseño Web</a></li>
+            <li><a class="text-muted" href="#">Desarrollo Web</a></li>
+            <li><a class="text-muted" href="#">Seguridad Informática</a></li>
+            <li><a class="text-muted" href="#">Redes</a></li>
+          </ul>
+        </div>
+        <div class="col-6 col-md">
+          <h5>Nosotros</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">Contacto</a></li>
+            <li><a class="text-muted" href="#">Soporte</a></li>
+            <li><a class="text-muted" href="#">Preguntas frecuentes</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <script>
+    $(document).ready(function() {
+      $("#saveContact").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+          url: 'guardar_contacto.php',
+          method: 'post',
+          data: $('form').serialize() + "&guardar_contacto=mensajeContacto",
+          success: function(response) {
+            $("#mensajeContacto").html(response);
+          }
         });
-    </script>
+      });
+    });
+  </script>
 </body>
-</html> 
+
+</html>
--- guardar_contacto.php
+++ guardar_contacto.php
@@ -1,37 +1,34 @@
 <?php
 require 'includes/connection.php';
-        var_dump($_POST['guardar_contacto']);
-if (isset($_POST['guardar_contacto']) && isset($_POST['guardar_contacto']) == 'mensajeContacto'){
-        $nombre = $_POST['nombre'];
-        var_dump($_POST['nombre']);
-        $email = $_POST['email'];
-        $telefono = $_POST['telefono'];
-        $comentario = $_POST['comentario'];
-       
-        $data = '';  
-        $stmt = $connection->prepare("INSERT INTO contacto (nombre,email,telefono,comentario) VALUES(?,?,?,?)");
-        $stmt->bind_param('sss', $nombre,$email,$telefono,$comentario);
-        $stmt->execute();        
+if (isset($_POST['guardar_contacto']) && isset($_POST['guardar_contacto']) == 'mensajeContacto') {
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $telefono = $_POST['telefono'];
+    $comentario = $_POST['comentario'];
+    $data = '';
+    $stmt = $connection->prepare("INSERT INTO contacto (nombre,email,telefono,comentario) VALUES(?,?,?,?)");
+    $stmt->bind_param('ssss', $nombre, $email, $telefono, $comentario);
+    $stmt->execute();
 
-        $stmt2 = $connection->prepare('SELECT * FROM contacto');
-        #$stmt2->bind_param('s', $_SESSION['idcliente']);
-        $stmt2->execute();
-        $results = $stmt2->get_result();
-        $contacto = null;
-        if ($results->num_rows > 0) {
-            while ($row = $results->fetch_assoc()) {
-                $contacto = $row;
-            }
+    $stmt2 = $connection->prepare('SELECT * FROM contacto WHERE telefono = ?');
+    $stmt2->bind_param('s', $telefono);
+    $stmt2->execute();
+    $results = $stmt2->get_result();
+    $contacto = null;
+    if ($results->num_rows > 0) {
+        while ($row = $results->fetch_assoc()) {
+            $contacto = $row;
         }
         $data .= '<div class="text-center">
-                                  <h1 class="display-4 mt-2 text-danger">Gracias por tu mensaje!</h1>
-                                  <h4>Nombre : ' . $contacto['nombre'] . '</h4>
-                                  <h4>Email : ' . $contacto['email'] . '</h4>
-                                  <h2 class="text-success">Hemos recibido tus comentarios correctamente!</h2>                                  
-                            </div>';
+            <h2 class="display-6 mt-2 text-success">Gracias por tu mensaje!</h2>                     
+            <h3 class="mt-2 text-info">' . $contacto['email'] . '</h3>
+            <h4 class="text-succes">Hemos recibido tus comentarios correctamente!</h4>                                  
+      </div>';
         echo $data;
-
-    }else{
-        echo "erro";
+    } else {
+        $data .= '<div class="text-center">           
+            <h2 class="text-danger">Error al recibir tus comentarios</h2>                                  
+      </div>';
+        echo $data;
     }
-?>
+}
